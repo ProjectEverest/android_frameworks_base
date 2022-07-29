b910c676b11241182f726e8adb1e237ceac802a6
cjybyjk <cjybyjk@zjnu.edu.cn>
2022-07-29 17:12:24 +0800

    [SQUASHED] base: Add status bar lyric for Android 14
    * This is a squash of:
    
    base: Add status bar lyric [1/3]
    
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Change-Id: I5c49187b0752286ee00a60d961203c0c4ec0d555
    
    fixup! base: Add status bar lyric [1/3]
    
     * java.lang.SecurityException: Settings key: <status_bar_show_lyric> is
       not readable. From S+, settings keys annotated with @hide are
       restricted to system_server and system apps only, unless they are
       annotated with @Readable.
    
    Change-Id: Ia0c209f3ddd5bbe1be676e172f408b3faf8cfe81
    
    base: Adapt StatusBar Lyric for Miku UI Udon
    
    Signed-off-by: AmeChanRain <downloadbot007@gmail.com>
    
    Co-authored-by: LynnrinChan <wuxiaofei31@gmail.com>
    Co-authored-by: AmeChanRain <downloadbot007@gmail.com>
    Change-Id: I55d8299268a633de9b8b9fcb1d9ebab7dbe901fc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -13134,6 +13134,18 @@ public final class Settings {
                 = "pocket_mode_enabled";
 
         /**
+         * Show lyric in status bar when playing music
+         * <ul>
+         *    <li> 0 = disabled </li>
+         *    <li> 1 = enabled </li>
+         * </ul>
+         *
+         * @hide
+         */
+        @Readable
+        public static final String STATUS_BAR_SHOW_LYRIC = "status_bar_show_lyric";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
