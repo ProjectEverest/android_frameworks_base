2565366cd3099d216e0d1db9a18e1a1bab74d6b4
cjybyjk <cjybyjk@zjnu.edu.cn>
2022-07-30 22:13:54 +0800

    base: Reset props for status bar lyric [1/2]
    Reset props to Meizu in some apps to use status bar lyric
    
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Change-Id: Idd573836af184b95ab0275f81fcb7ae3057783b9
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -71,6 +71,8 @@ import java.util.Objects;
 import java.util.StringJoiner;
 import java.util.concurrent.TimeoutException;
 
+import com.android.internal.util.everest.MeizuPropsUtils;
+
 /**
  * Base class for implementing application instrumentation code.  When running
  * with instrumentation turned on, this class will be instantiated for you
@@ -1344,6 +1346,7 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
+        MeizuPropsUtils.setProps(app);
         return app;
     }
     
@@ -1361,6 +1364,7 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
+        MeizuPropsUtils.setProps(app);
         return app;
     }
 
