78358e946eb6211db2a5a08c45241b74a76de475
SamarV-121 <samarvispute121@pm.me>
2023-01-10 22:17:04 -0600

    base: Add settings to change QS tile shape [1/2]
    Change-Id: Ia8919fa755e5c865fc14cbeb9f4312e83cdbb750

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12976,6 +12976,12 @@ public final class Settings {
          */
         public static final String QS_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
 
+	/**
+         * Quicksettings tile shape
+         * @hide
+         */
+        public static final String QS_TILE_SHAPE = "qs_tile_shape";
+
         /**
          * Control whether FLAG_SECURE is ignored for all windows.
          * @hide
