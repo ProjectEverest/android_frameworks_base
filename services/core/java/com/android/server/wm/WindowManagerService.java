f07775d3c221424a20fcf8133142418ebf4bcf82
minaripenguin <minaripenguin@users.noreply.github.com>
2023-12-04 08:22:34 +0800

    wm: Always account snapshot and IME surface when determining IME visbility
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -9485,6 +9485,7 @@ public class WindowManagerService extends IWindowManager.Stub
 
     boolean shouldRestoreImeVisibility(IBinder imeTargetWindowToken) {
         final Task imeTargetWindowTask;
+        boolean wasLastShown = false;
         synchronized (mGlobalLock) {
             final WindowState imeTargetWindow = mWindowMap.get(imeTargetWindowToken);
             if (imeTargetWindow == null) {
@@ -9496,13 +9497,13 @@ public class WindowManagerService extends IWindowManager.Stub
             }
             if (imeTargetWindow.mActivityRecord != null
                     && imeTargetWindow.mActivityRecord.mLastImeShown) {
-                return true;
+                wasLastShown = true;
             }
         }
         final TaskSnapshot snapshot = getTaskSnapshot(imeTargetWindowTask.mTaskId,
                 imeTargetWindowTask.mUserId, false /* isLowResolution */,
                 false /* restoreFromDisk */);
-        return snapshot != null && snapshot.hasImeSurface();
+        return snapshot != null && (snapshot.hasImeSurface() || wasLastShown);
     }
 
     @Override
