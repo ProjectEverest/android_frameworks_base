f29234b9b3239e0c5014007a24c058ce2434217c
Subratam6296 <padraramesh420@gmail.com>
2024-03-09 10:10:49 +0530

    NetworkTraffic: Allow to disable Up/Down indicators [1/2]
    Signed-off-by: Subratam6296 <padraramesh420@gmail.com>
    Change-Id: I6433fb52779b7f4c62992c531d5917c3012a5407

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6380,6 +6380,12 @@ public final class Settings {
          * @hide
          */
         public static final String NETWORK_TRAFFIC_UNIT_TYPE = "network_traffic_unit_type";
+        
+        /**
+         * Up/Down indicator preference for network traffic
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_DRAWABLE = "network_traffic_drawable";
 
         /**
          * Whether the torch launch gesture to double tap or long press the power button when the
